eb4d6df04526a51524fac7fb730e7230c80c4303
cacosandon <jiossandon@uc.cl>
2021-11-01 11:35:37 -0300

    Initial Commit

---
+++ Shared/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Shared
+//
+//  Created by Caco Ossandon on 01-11-21.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
